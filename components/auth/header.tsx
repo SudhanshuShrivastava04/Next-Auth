8bfd5f985b0faed78d1ff83e801276ad1dfffdc6
Sudhanshu Shrivastava <211230053@nitdelhi.ac.in>
2024-03-29 10:22:22 +0530

    feat: Login form ui and schema

--- components/auth/header.tsx
+++ components/auth/header.tsx
@@ -14,7 +14,7 @@ const font = Poppins({
 const Header: FC<headerProps> = ({ lable }) => {
   return (
     <div className="w-full flex flex-col gap-y-4 items-center justify-center">
-      <h1 className={cn("text-3xl font-semibold", font.className)}>Auth</h1>
+      <h1 className={cn("text-3xl font-semibold", font.className)}>Verify Me</h1>
       <p className="text-muted-foreground text-sm">{lable}</p>
     </div>
   );
--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -1,9 +1,42 @@
-import { FC } from "react";
+"use client";
+
+import { FC, useState } from "react";
 import CardWrapper from "./card-wrapper";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { LoginSchema } from "@/schemas";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
+import FormError from "../form-error";
+import FormSuccess from "../form-success";
 
 interface LoginFormProps {}
 
 const LoginForm: FC<LoginFormProps> = ({}) => {
+  const [visible, setVisible] = useState(false);
+
+  const form = useForm<z.infer<typeof LoginSchema>>({
+    resolver: zodResolver(LoginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof LoginSchema>) => {
+    console.log("Submitted : ", values);
+  };
+
   return (
     <CardWrapper
       headerLabel={"Welcome Back"}
@@ -11,7 +44,62 @@ const LoginForm: FC<LoginFormProps> = ({}) => {
       backButtonhref={"/auth/register"}
       showSocial
     >
-      login-form
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+          <div className="space-y-4">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      placeholder="john.doe@emaxple.com"
+                      type="email"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <div className="w-full flex space-x-2">
+                      <Input
+                        {...field}
+                        placeholder="******"
+                        type={visible ? "text" : "password"}
+                      />
+
+                      <Button
+                        variant="outline"
+                        type="button"
+                        onClick={() => setVisible((prev) => !prev)}
+                      >
+                        {visible ? <AiOutlineEye /> : <AiOutlineEyeInvisible />}
+                      </Button>
+                    </div>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <FormError message="Something went wrong!" />
+          <FormSuccess message="Email sent!" />
+          <Button className="w-full" type="submit">
+            Login
+          </Button>
+        </form>
+      </Form>
     </CardWrapper>
   );
 };
