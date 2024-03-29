dfcff68056686726d672f5458f63345f2c2a6b45
Sudhanshu Shrivastava <211230053@nitdelhi.ac.in>
2024-03-29 21:01:23 +0530

    feat(login): middlewares, login and signup

--- actions/login.ts
+++ actions/login.ts
@@ -2,6 +2,9 @@
 
 import { LoginSchema } from "@/schemas";
 import { z } from "zod";
+import { signIn } from "@/auth";
+import { AuthError } from "next-auth";
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
   const validateFields = LoginSchema.safeParse(values);
@@ -9,6 +12,26 @@ export const login = async (values: z.infer<typeof LoginSchema>) => {
     return { error: "Invalid field!" };
   }
 
-  return { success: "Email sent!" };
+  const { email, password } = validateFields.data;
+  try {
+    await signIn("credentials", {
+      email,
+      password,
+      redirectTo: DEFAULT_LOGIN_REDIRECT,
+    });
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return { error: "Invaild Credentials!" };
+
+        default:
+          return { error: "Something went wrong!" };
+      }
+    }
+
+    throw error;
+  }
+
   console.log(values); // action for server side console log (check terminal)
 };
---
+++ app/(protected)/settings/page.tsx
@@ -0,0 +1,26 @@
+import { auth, signOut } from "@/auth";
+import { Button } from "@/components/ui/button";
+import { FC } from "react";
+
+interface SettingsPageProps {}
+
+const SettingsPage: FC<SettingsPageProps> = async ({}) => {
+  const session = await auth();
+
+  return (
+    <div>
+      {JSON.stringify(session)}
+      <form
+        action={async () => {
+          "use server";
+
+          await signOut();
+        }}
+      >
+        <Button>Sign Out</Button>
+      </form>
+    </div>
+  );
+};
+
+export default SettingsPage;
