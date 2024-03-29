4a65f8cbd55e29447badf4d8f91fcde04b792762
Sudhanshu Shrivastava <211230053@nitdelhi.ac.in>
2024-03-29 12:44:49 +0530

    User created in database by register

--- actions/register.ts
+++ actions/register.ts
@@ -1,7 +1,10 @@
 "use server";
 
+import bcrypt from "bcrypt";
 import { RegisterSchema } from "@/schemas";
 import { z } from "zod";
+import { db } from "@/lib/db";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validateFields = RegisterSchema.safeParse(values);
@@ -9,6 +12,24 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
     return { error: "Invalid field!" };
   }
 
-  return { success: "Email sent!" };
+  const { email, name, password } = validateFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { error: "Email already in use" };
+  }
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  // TODO: Send verification token email
+
+  return { success: "User created!" };
   console.log(values); // action for server side console log (check terminal)
 };
